46e00d3b08b8975d136246692c243018a5f8f375
FrankNPC <cmings.chen@gmail.com>
2025-08-13 10:30:56 -0400

    bugfix

--- src/main/java/io/steplogs/spring/datasource/dual/Write.java
+++ src/main/java/io/steplogs/spring/datasource/dual/Write.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface Write {
 	
 }
